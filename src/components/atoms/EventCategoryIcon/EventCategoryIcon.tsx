4b30d20d71d1e852ff383cf66b75b9ae71a1b413
gyusu <kyusu88@naver.com>
2022-07-17 21:29:59 +0900

    feat(icon): 아이콘모듈 변경(react-icons)

---
+++ src/components/atoms/EventCategoryIcon/EventCategoryIcon.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { IconType } from 'react-icons/lib';
+
+function IconBody(props: { icon: IconType }) {
+  const Icon = props.icon;
+  return <Icon size={32} />;
+}
+
+export function EventCategoryIcon(props: { icon: IconType }) {
+  return (
+    <Wrapper>
+      <div className="icon">
+        <IconBody icon={props.icon} />
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: block;
+  margin-right: 50px;
+  .icon {
+    padding: 5px;
+  }
+  .icon:hover {
+    color: red;
+    border-bottom: solid;
+    border-color: red;
+  }
+`;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
+import { MainBody } from '../components/organisms/MainBody/MainBody';
 import { MainHeader } from '../components/organisms/MainHeader/MainHeader';
 
 export function MainPage() {
   return (
     <div>
       <MainHeader />
+      <MainBody />
     </div>
   );
 }
