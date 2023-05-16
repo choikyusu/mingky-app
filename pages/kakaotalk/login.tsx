cfbe958ef3b92c806f72a64334a46072287acf56
최규수 <kyusu88@naver.com>
2023-05-16 22:31:16 +0900

    feat(kakao): global css 추가

--- pages/kakaotalk/login.tsx
+++ pages/kakaotalk/login.tsx
@@ -8,6 +8,20 @@ const Login = () => {
         <Styled.Header>
           <img src="/asset/kakao_logo.png" alt="logo" />
         </Styled.Header>
+        <Styled.Contents>
+          <form>
+            <input type="text" placeholder="계정" />
+            <input
+              type="password"
+              autoComplete="new-password"
+              placeholder="비밀번호"
+            />
+            <button type="button" className="disabled">
+              로그인
+            </button>
+            <p> </p>
+          </form>
+        </Styled.Contents>
         <Styled.Footer>
           <ul>
             <li>
@@ -44,6 +58,73 @@ const Styled = {
       margin: 0 auto;
     }
   `,
+  Contents: styled.div`
+    width: 100%;
+    height: 330px;
+    padding-top: 30px;
+    & input {
+      display: block;
+      margin: 0 auto;
+      padding: 10px 5px;
+      width: 230px;
+      border: 1px solid #dcdcdc;
+      &:first-child {
+        border-bottom: none;
+      }
+      &::placeholder {
+        color: #a2a2a2;
+      }
+    }
+    & button {
+      position: relative;
+      display: block;
+      margin: auto;
+      margin-top: 5px;
+      padding: 10px 5px;
+      width: 230px;
+      border: 1px solid #000;
+      color: #fff;
+      background-color: #423630;
+      outline: none;
+      @keyframes iconLotate {
+        0% {
+          transform: rotate(0deg);
+        }
+        50% {
+          transform: rotate(180deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+      & i {
+        position: absolute;
+        top: 15px;
+        right: 10px;
+        color: #5c5c5c;
+        animation: iconLotate 1.5s linear infinite;
+      }
+      &:hover {
+        background-color: #594941;
+        cursor: pointer;
+      }
+      &:active {
+        background-color: #423630;
+      }
+      &.disabled {
+        color: #969696;
+        background: #e2e2e2;
+        pointer-events: none;
+        border: 1px solid #dcdcdc;
+      }
+    }
+    & p {
+      padding-top: 20px;
+      text-align: center;
+      color: red;
+    }
+  `,
+
   Footer: styled.footer`
     & ul {
       display: flex;
