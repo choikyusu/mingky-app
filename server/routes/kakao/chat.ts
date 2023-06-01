e138e8df18bd1ba9cac379c3271fda7706303450
최규수 <kyusu88@naver.com>
2023-06-01 09:02:35 +0900

    refactor: router 고도화

--- server/routes/kakao/chat.ts
+++ server/routes/kakao/chat.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import jwtToken, { TOKEN_EXPIRED } from '../../auth/kakao/jwtToken';
 import { User } from '../../schemas/kakao/user';
 import { Room } from '../../schemas/kakao/room';
 import { Message } from '../../schemas/kakao/message';
@@ -8,7 +7,7 @@ import { Types } from 'mongoose';
 
 const router = express.Router();
 
-router.post('/room/create', async (req, res) => {
+router.post('/room/create', async (req: any, res) => {
   const {
     roomInfo,
   }: {
@@ -19,109 +18,84 @@ router.post('/room/create', async (req, res) => {
       participantList: string[];
     };
   } = req.body;
+  const { userId } = req;
 
-  if (
-    req.headers.authorization &&
-    typeof req.headers.authorization === 'string'
-  ) {
-    const token = req.headers.authorization.split('Bearer ')[1];
-    const result = jwtToken.verify(token);
-    if (result.ok && result.userId) {
-      const user = await User.findOne({ userId: result.userId });
-      const room = await Room.findOne({ identifier: roomInfo.identifier });
-      if (user) {
-        if (!room) {
-          const participantIdList = roomInfo.participantList.map(
-            async userId => {
-              const user = await User.findOne({ userId });
-              if (user) {
-                const participant = await Participant.create({
-                  identifier: roomInfo.identifier,
-                  lastReadChatNo: 0,
-                  newChat: 0,
-                  roomName: roomInfo.roomName,
-                  userObjectId: user._id,
-                  userId: user.userId,
-                });
-
-                return participant._id;
-              }
-              return undefined;
-            },
-          );
-
-          const participantResult = (
-            await Promise.all(participantIdList)
-          ).filter(
-            participantId => participantId !== undefined,
-          ) as Types.ObjectId[];
-
-          await Room.create({
+  const user = await User.findOne({ userId });
+  const room = await Room.findOne({ identifier: roomInfo.identifier });
+  if (user) {
+    if (!room) {
+      const participantIdList = roomInfo.participantList.map(async userId => {
+        const user = await User.findOne({ userId });
+        if (user) {
+          const participant = await Participant.create({
             identifier: roomInfo.identifier,
-            type: roomInfo.type,
-            messageList: [],
-            lastChat: '',
-            participantList: participantResult,
+            lastReadChatNo: 0,
+            newChat: 0,
+            roomName: roomInfo.roomName,
+            userObjectId: user._id,
+            userId: user.userId,
           });
+
+          return participant._id;
         }
+        return undefined;
+      });
 
-        const createdRoom = await Room.findOne({
-          identifier: roomInfo.identifier,
-        }).populate({
-          path: 'participantList',
-          select: 'userId',
-        });
+      const participantResult = (await Promise.all(participantIdList)).filter(
+        participantId => participantId !== undefined,
+      ) as Types.ObjectId[];
 
-        const myRoomInfo = await Participant.findOne({
-          identifier: roomInfo.identifier,
-          userId: result.userId,
-        });
-
-        console.log('myRoomInfo', myRoomInfo);
-
-        return res.json({
-          data: {
-            identifier: createdRoom?.identifier || '',
-            type: createdRoom?.type || '',
-            roomName: myRoomInfo?.roomName || '',
-            participantList: createdRoom?.participantList,
-          },
-        });
-      }
-
-      return res.status(404).json({ msg: 'user not exist' });
+      await Room.create({
+        identifier: roomInfo.identifier,
+        type: roomInfo.type,
+        messageList: [],
+        lastChat: '',
+        participantList: participantResult,
+      });
     }
-    if (!result.ok && result.error === TOKEN_EXPIRED) {
-      return res.status(401).json({ msg: 'token_expired' });
-    }
+
+    const createdRoom = await Room.findOne({
+      identifier: roomInfo.identifier,
+    }).populate({
+      path: 'participantList',
+      select: 'userId',
+    });
+
+    const myRoomInfo = await Participant.findOne({
+      identifier: roomInfo.identifier,
+      userId,
+    });
+
+    console.log('myRoomInfo', myRoomInfo);
+
+    return res.json({
+      data: {
+        identifier: createdRoom?.identifier || '',
+        type: createdRoom?.type || '',
+        roomName: myRoomInfo?.roomName || '',
+        participantList: createdRoom?.participantList,
+      },
+    });
   }
-  return res.status(400).json({ msg: 'cannot find token' });
+
+  return res.status(404).json({ msg: 'user not exist' });
 });
 
-router.get('/room', async (req, res) => {
+router.get('/room', async (req: any, res) => {
   const identifier = req.query.identifier as string;
-  if (
-    req.headers.authorization &&
-    typeof req.headers.authorization === 'string'
-  ) {
-    const token = req.headers.authorization.split('Bearer ')[1];
-    const result = jwtToken.verify(token);
-    if (result.ok && result.userId) {
-      const user = await User.findOne({ userId: result.userId }).select(
-        'userId name nickName message profileUrl backgroundUrl friendList',
-      );
+  const { userId } = req;
 
-      if (user) {
-        const messageList = await Message.find({ identifier }).sort('index');
-        return res.json({
-          data: messageList,
-        });
-      }
-    }
-    if (!result.ok && result.error === TOKEN_EXPIRED) {
-      return res.status(401).json({ data: false, msg: 'token_expired' });
-    }
+  const user = await User.findOne({ userId }).select(
+    'userId name nickName message profileUrl backgroundUrl friendList',
+  );
+
+  if (user) {
+    const messageList = await Message.find({ identifier }).sort('index');
+    return res.json({
+      data: messageList,
+    });
   }
+
   return res.status(500).json({ msg: '사용자 정보를 찾지 못했습니다.' });
 });
 
--- server/routes/kakao/friend.ts
+++ server/routes/kakao/friend.ts
@@ -1,42 +1,29 @@
 import express from 'express';
-import jwtToken, { TOKEN_EXPIRED } from '../../auth/kakao/jwtToken';
 import { User } from '../../schemas/kakao/user';
 
 const router = express.Router();
 
-router.post('/add', async (req, res) => {
-  const { body } = req;
-  if (
-    req.headers.authorization &&
-    typeof req.headers.authorization === 'string'
-  ) {
-    const token = req.headers.authorization.split('Bearer ')[1];
-    const result = jwtToken.verify(token);
-    if (result.ok && result.userId) {
-      if (result.userId === body.friendId)
-        return res.status(400).json({ msg: 'Cannot add yourself as a friend' });
+router.post('/add', async (req: any, res) => {
+  const { body, userId } = req;
 
-      const user = await User.findOne({ userId: result.userId });
-      const friend = await User.findOne({ userId: body.friendId });
-      if (user && friend) {
-        if (!user.friendList.includes(friend._id)) {
-          await User.updateOne(
-            { userId: result.userId },
-            {
-              friendList: [...user.friendList, friend._id],
-            },
-          );
-          return res.json({ data: true, msg: '친구 추가' });
-        }
-        return res.status(409).json({ msg: 'Friend already added' });
-      }
-      return res.status(404).json({ msg: 'cannot add friend' });
+  if (userId === body.friendId)
+    return res.status(400).json({ msg: 'Cannot add yourself as a friend' });
+
+  const user = await User.findOne({ userId });
+  const friend = await User.findOne({ userId: body.friendId });
+  if (user && friend) {
+    if (!user.friendList.includes(friend._id)) {
+      await User.updateOne(
+        { userId },
+        {
+          friendList: [...user.friendList, friend._id],
+        },
+      );
+      return res.json({ data: true, msg: '친구 추가' });
     }
-    if (!result.ok && result.error === TOKEN_EXPIRED) {
-      return res.status(401).json({ msg: 'token_expired' });
-    }
+    return res.status(409).json({ msg: 'Friend already added' });
   }
-  return res.status(400).json({ msg: 'cannot find token' });
+  return res.status(404).json({ msg: 'cannot add friend' });
 });
 
 export default router;
